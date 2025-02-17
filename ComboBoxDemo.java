212a7b4d1d92d4dc56a8e9a6a23cdaf44347dd5f
Max Vanderbeek <mk27vand@siena.edu>
2025-02-17 10:48:58 -0500

    Added <String> to the beggining of the constructor to fix it

--- ComboBoxDemo.java
+++ ComboBoxDemo.java
@@ -18,7 +18,7 @@ class ComboBoxDemo extends JFrame implements ItemListener {
 	static JLabel label, label1;
 
 	// combobox
-	static JComboBox cBox1;
+	static JComboBox<String> cBox1;
 
 	// main class
 	public static void main(String[] args)
@@ -36,11 +36,9 @@ class ComboBoxDemo extends JFrame implements ItemListener {
 		// set layout of frame
 		frame.setLayout(new FlowLayout());
 
-		// array of string containing cities
-		String s1[] = { "Albany", "Schenectady", "Troy", "Glens Falls", "Catskill" };
-
+		String[] s1 = {"Albany", "Schenectady", "Troy", "Glens Falls", "Catskill"};
 		// create checkbox
-        cBox1 = new JComboBox(s1);
+        cBox1 = new JComboBox<>(s1);
 		
 		// add ItemListener
 		cBox1.addItemListener(s);
